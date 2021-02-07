2a96759fc73870de3411d456dd5585b09ebcaceb
wll8090 <54913374+wll8090@users.noreply.github.com>
2021-02-07 19:24:22 -0300

    Create index.py
    apenas um teste

---
+++ index.py
@@ -0,0 +1,10 @@
+print('ola mundo')
+print('apenas uma teste no gitehub')
+var=input('digite algo')
+print(f'voce digitou: {var}')
+if type(var)==int:
+  print(f'{var} é um numero')
+elif type(var)==str:
+  print(f'{var} é uma str')
+esle:
+  print(f'{var} não é str ou int')
